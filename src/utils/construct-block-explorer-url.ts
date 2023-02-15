9b96443ce06a8f9d53aeb7bb59597437ce18334d
ooithianhooi <ooithianhooi@gmail.com>
2023-02-15 23:21:38 +0800

    feat: added `constructBlockExplorerUrl` util

---
+++ src/utils/construct-block-explorer-url.ts
@@ -0,0 +1,16 @@
+import { ChainId } from "@thirdweb-dev/sdk";
+
+const Web3ChainToBlockExplorer: Record<string, string> = {
+  [ChainId.Mainnet]: "https://etherscan.io/",
+  [ChainId.Goerli]: "https://goerli.etherscan.io/",
+  [ChainId.Polygon]: "https://polygonscan.com/",
+  [ChainId.Mumbai]: "https://mumbai.polygonscan.com/",
+};
+
+export const constructBlockExplorerUrl = (
+  network: ChainId,
+  address: string
+) => {
+  const blockExplorerUrl = Web3ChainToBlockExplorer[network];
+  return `${blockExplorerUrl}/address/${address}`;
+};
